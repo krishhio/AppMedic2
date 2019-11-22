acaaf86432776975b0265fd655f860aff8d74490
misha28x <misha1198x@gmail.com>
2019-11-22 17:58:00 +0200

    revorked form for adding doctor

--- src/pages/Doctors/DoctorForm.tsx
+++ src/pages/Doctors/DoctorForm.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { IUser } from '../../interfaces/user';
-import { useFormik } from 'formik';
-import ImageLoader from '../../layout/components/Patients/ImageLoader';
+import { useFormik, Form, FieldArray, Field, Formik } from 'formik';
 import { Input, Select } from 'antd';
+
+import ImageLoader from '../../layout/components/Patients/ImageLoader';
 
 const { TextArea } = Input;
 
@@ -11,83 +12,96 @@ type Props = {
   onCancel: () => void;
 };
 
-const DoctorForm = ({ onSubmit, onCancel }: Props) => {
-  const { handleSubmit, values, handleChange, setFieldValue } = useFormik<IUser>({
-    initialValues: {
-      social: [],
-      profileLink: '',
-      role: '',
-      name: '',
-      lastName: '',
-      img: '',
-      gender: '',
-      address: ''
+const initialValues = {
+  social: [
+    {
+      icon: '',
+      link: ''
     },
-    onSubmit: onSubmit
-  });
+    {
+      icon: '',
+      link: ''
+    },
+    {
+      icon: '',
+      link: ''
+    }
+  ],
+  profileLink: '',
+  role: '',
+  name: '',
+  lastName: '',
+  img: '',
+  gender: '',
+  address: ''
+};
 
-  const handleGenderSelect = value => setFieldValue('gender', value);
-
+const DoctorForm = ({ onSubmit, onCancel }: Props) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div className='form-group'>
-        <ImageLoader src={`${window.location.origin}/${values.img}`} />
-      </div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      render={({ values, handleChange }) => (
+        <Form>
+          <div className='form-group'>
+            <ImageLoader src={`${window.location.origin}/${values.img}`} />
+          </div>
 
-      <div className='form-group'>
-        <Input
-          placeholder='First name'
-          name='name'
-          type='text'
-          defaultValue={values.name}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className='form-group'>
-        <Input
-          placeholder='Last name'
-          name='lastName'
-          type='text'
-          defaultValue={values.lastName}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className='row'>
-        <div className='col-sm-6 col-12'>
           <div className='form-group'>
             <Input
-              placeholder='Speciality'
-              name='role'
+              placeholder='First name'
+              name='name'
               type='text'
-              defaultValue={values.role}
+              defaultValue={values.name}
               onChange={handleChange}
             />
           </div>
-        </div>
 
-        <div className='col-sm-6 col-12'>
           <div className='form-group'>
-            <Select defaultValue={values.gender} onChange={handleGenderSelect}>
-              <Select.Option value='Male'>Male</Select.Option>
-              <Select.Option value='Female'>Female</Select.Option>
-            </Select>
+            <Input
+              placeholder='Last name'
+              name='lastName'
+              type='text'
+              defaultValue={values.lastName}
+              onChange={handleChange}
+            />
           </div>
-        </div>
-      </div>
 
+          <div className='row'>
+            <div className='col-sm-6 col-12'>
+              <div className='form-group'>
+                <Input
+                  placeholder='Speciality'
+                  name='role'
+                  type='text'
+                  defaultValue={values.role}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
 
-      <div className='form-group'>
-        <TextArea
-          rows={3}
-          placeholder='Address'
-          name='address'
-          defaultValue={values.address}
-          onChange={handleChange}
-        />
-      </div>
-    </form>
+            <div className='col-sm-6 col-12'>
+              <div className='form-group'>
+                <Select defaultValue={values.gender} onChange={handleGenderSelect}>
+                  <Select.Option value='Male'>Male</Select.Option>
+                  <Select.Option value='Female'>Female</Select.Option>
+                </Select>
+              </div>
+            </div>
+          </div>
+
+          <div className='form-group'>
+            <TextArea
+              rows={3}
+              placeholder='Address'
+              name='address'
+              defaultValue={values.address}
+              onChange={handleChange}
+            />
+          </div>
+        </Form>
+      )}
+    />
   );
 };
 
