b6805f2ce47fe3dc34dab8bc95c1bbf385ddc6d0
misha28x <misha1998x@gmail.com>
2020-07-31 18:24:24 +0300

    finished department component

--- src/layout/components/Department/Department.tsx
+++ src/layout/components/Department/Department.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Avatar, Button, Card } from 'antd';
+import { FileDoneOutlined, PlusOutlined } from '@ant-design/icons/lib';
 
-type Props = {};
+type Props = { dep: any };
 
-const Department = function ({}: Props) {
+const Department = ({ dep }: Props) => (
+  <>
+    <Card className='department'>
+      <h3 className='h4 mt-0'>{dep.title}</h3>
 
-};
+      <div className='team d-flex align-items-center mb-4'>
+        <strong className='mr-3'>Team:</strong>
+
+        {dep.team.map((src) => (
+          <Avatar src={src} />
+        ))}
+        <Button shape='circle' icon={<PlusOutlined />} />
+      </div>
+
+      <p>{dep.desc}</p>
+
+      <div className='button-box pb-2'>
+        <Button>
+          More <FileDoneOutlined className='ml-2' />
+        </Button>
+      </div>
+    </Card>
+  </>
+);
 
 export default Department;
