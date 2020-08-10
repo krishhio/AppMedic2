f973c275184aa959894666baf702fe366c570399
misha28x <misha1998x@gmail.com>
2020-08-10 13:09:03 +0300

    fixed breadcrumbs redirect bug

--- src/layout/components/Footer/Breadcrumbs.tsx
+++ src/layout/components/Footer/Breadcrumbs.tsx
@@ -3,23 +3,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { IBreadcrumb } from '../../../interfaces/page';
+import ReactEchartsCore from 'echarts-for-react/lib/core';
 
 type Props = {
   breadcrumbs: IBreadcrumb[];
   layout: string;
 };
 
-const Breadcrumbs = ({ breadcrumbs = [], layout }: Props) => {
-  const breadcrumbList = breadcrumbs.map((item: IBreadcrumb, index) => (
-    <li className='item' key={index}>
-      {item.route ? (
-        <NavLink to={`${layout}/${item.route}`}>{item.title}</NavLink>
+const Breadcrumb = ({ route, title, layout }) => {
+  return (
+    <li className='item'>
+      {route ? (
+        <NavLink replace to={`/${layout}/${route}`}>
+          {title}
+        </NavLink>
       ) : (
-        <span>{item.title}</span>
+        <span>{title}</span>
       )}
-
-      {index < breadcrumbs.length - 1 && <i className='separator icofont icofont-thin-right' />}
     </li>
+  );
+};
+
+const Breadcrumbs = ({ breadcrumbs = [], layout }: Props) => {
+  const breadcrumbList = breadcrumbs.map(({ title, route }: IBreadcrumb, index) => (
+    <React.Fragment key={index}>
+      <Breadcrumb title={title} route={route} layout={layout} />
+      {index < breadcrumbs.length - 1 && <i className='separator icofont icofont-thin-right' />}
+    </React.Fragment>
   ));
 
   return <ul className='page-breadcrumbs'>{breadcrumbList}</ul>;
