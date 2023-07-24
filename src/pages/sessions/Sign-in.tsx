513e5c5d2b1e96b966fe07076e0141625fc37eb3
Krishhio <cristian@issivs.com>
2023-07-23 23:24:10 -0600

    Cambio de idioma en intarfaz de inicio

--- src/pages/sessions/Sign-in.tsx
+++ src/pages/sessions/Sign-in.tsx
@@ -19,20 +19,20 @@ const SignIn = () => {
 
   return (
     <PublicLayout bgImg={`${window.origin}/content/login-page.jpg`}>
-      <h4 className='mt-0 mb-1'>Login form</h4>
+      <h4 className='mt-0 mb-1'>Bienvenido a DrHelp</h4>
 
-      <p className='text-color-200'>Login to access your Account</p>
+      <p className='text-color-200'>Inicia Sesion a tu cuenta.</p>
 
       <Form form={form} layout='vertical' className='mb-4'>
         <Form.Item name='login' rules={[{ required: true, message: <></> }]}>
-          <Input placeholder='Login' />
+          <Input placeholder='Nombre de Usuario' />
         </Form.Item>
         <Form.Item name='password' rules={[{ required: true, message: <></> }]}>
-          <Input placeholder='Password' type='password' />
+          <Input placeholder='Contraseña' type='password' />
         </Form.Item>
 
         <div className='d-flex align-items-center mb-4'>
-          <Switch defaultChecked /> <span className='ml-2'>Remember me</span>
+          <Switch defaultChecked /> <span className='ml-2'>Recuerdame</span>
         </div>
 
         <Button
@@ -47,11 +47,11 @@ const SignIn = () => {
       </Form>
       <br />
       <p className='mb-1'>
-        <a href='#'>Forgot password?</a>
+        <a href='#'>¿Olvidaste Contraseña?</a>
       </p>
 
       <p>
-        Don't have an account? <Link to='../sign-up'>Sign up!</Link>
+        ¿No tienes una cuenta? <Link to='../sign-up'>Registrate!</Link>
       </p>
     </PublicLayout>
   );
